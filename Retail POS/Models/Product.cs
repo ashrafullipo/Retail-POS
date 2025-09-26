d4379f6454df3765d2755851f94c2fa282fe8b5f
Ashraful Islam <ashrafullipo@gmail.com>
2025-09-26 23:27:52 +0600

    Add project files.

---
+++ Retail POS/Models/Product.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Retail_POS.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+
+        [Required(ErrorMessage = "Product Name is required")]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string SKU { get; set; }
+
+        [Required(ErrorMessage = "Price is required")]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Quantity is required")]
+        public int Quantity { get; set; }
+
+        // Instead of foreign key, store category as string
+        [StringLength(50)]
+        public string Category { get; set; }
+
+        [NotMapped]
+        public decimal TotalValue => Price * Quantity;
+    }
+}
