e0c17ffea57579743cf5ee3267c5da0872be264f
Ashraful Islam <ashrafullipo@gmail.com>
2025-09-28 20:02:15 +0600

    Add Feature: Purchase, New Purchase, Purchase History, withs Bootstrap desing.

--- Retail POS/Models/Purchase.cs
+++ Retail POS/Models/Purchase.cs
@@ -12,19 +12,20 @@ namespace Retail_POS.Models
         public int SupplierId { get; set; }
 
         [ForeignKey("SupplierId")]
-        public Supplier Supplier { get; set; }
+        public Supplier? Supplier { get; set; }
 
         [Required]
         public DateTime PurchaseDate { get; set; } = DateTime.Now;
 
-        [Required]
         [Column(TypeName = "decimal(18,2)")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Amount must be greater than 0")]
         public decimal TotalAmount { get; set; }
+
 
         [StringLength(255)]
         public string? Notes { get; set; }
 
         // Navigation property
-        public  ICollection<PurchaseItem> PurchaseItems { get; set; }
+        public  ICollection<PurchaseItem> PurchaseItems { get; set; } = new List<PurchaseItem>();
     }
 }
