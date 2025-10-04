4f7ab57c35b5b89328eb2743f499a7b64557401f
Ashraful Islam <ashrafullipo@gmail.com>
2025-10-04 15:40:51 +0600

    added view model, views

---
+++ Retail POS/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Retail_POS.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //Login
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+
+        //Register
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ Retail POS/Controllers/DashboardController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Retail_POS.Data;
+
+namespace Retail_POS.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private readonly AppDbContext context;
+
+        public DashboardController (AppDbContext context)
+        {
+            this.context = context;
+        }
+
+
+
+        public IActionResult Index()
+        {
+            var totalProducts = context.Products.Count();
+            var totalCustomer = context.Customers.Count();
+            var totalSuppliers = context.Suppliers.Count();
+            var totalSales = context.Sales.Count();
+
+
+            ViewBag.TotalProducts = totalProducts;  
+            ViewBag.TotalCustomers = totalCustomer;
+            ViewBag.TotalSuppliers = totalSuppliers;
+            ViewBag.TotalSales = totalSales;
+
+
+            return View();
+        }
+    }
+}
