0d383c7749efe41ef80741969ed8822e1c2bfee5
Ashraful Islam <ashrafullipo@gmail.com>
2025-10-05 15:41:14 +0600

    Login method successfull

--- Retail POS/Controllers/AccountController.cs
+++ Retail POS/Controllers/AccountController.cs
@@ -1,25 +1,81 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Retail_POS.Models;
+using Retail_POS.ViewModel;
 
 namespace Retail_POS.Controllers
 {
     public class AccountController : Controller
     {
+
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly SignInManager<ApplicationUser> signInManager;
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, RoleManager<IdentityRole> roleManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+            this.roleManager = roleManager;
+        }
+
+
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
 
         //Login
+        [HttpGet]
         public IActionResult Login()
         {
             return View();
         }
 
 
-        //Register
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (model != null)
+            {
+                var usr = await userManager.FindByEmailAsync(model.Email);
+                if (usr != null)
+                {
+                    var res = await signInManager.PasswordSignInAsync(usr, model.Password, false, false);
+                    if (res.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        TempData["Message"] = "Invalid Password";
+                    }
+                }
+                else
+                {
+                    TempData["Message"] = "Invalid Email";
+                }
+            }
+            else
+            {
+                TempData["Message"] = "Something went wrong";
+            }
+
+            return View(model);
+        }
+
+
+
+
+
         public IActionResult Register()
         {
             return View();
         }
+
+
+        //Register
+        
     }
 }
